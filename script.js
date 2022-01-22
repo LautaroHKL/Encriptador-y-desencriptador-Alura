f58c39820569d6a05da759bc14e9898f0ecdca0c
Lautaro <elchicharra82@gmail.com>
2022-01-22 17:59:02 -0300

    mejorada la logica de encriptación

--- script.js
+++ script.js
@@ -21,68 +21,44 @@ No se permite acentuación de palabras
 
 
 function encriptar (){
-        var texto = document.querySelector("#input-texto").value;
-        var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-        document.querySelector(".text-input-salida").value = textoCifrado;
-        document.querySelector("#input-texto").value;
-        
+    var texto = document.querySelector("#input-texto").value;
 
+    let textoCifrado = "";
+
+        for (var i = 0; i < texto.length; i++) {
+         
+            if (texto[i] == "e") {
+                textoCifrado = textoCifrado + "enter";
+            } else if (texto[i] == "i") {
+                textoCifrado = textoCifrado + "imes";
+            } else if (texto[i] == "a") {
+                textoCifrado = textoCifrado + "ai";
+            } else if (texto[i] == "o") {
+                textoCifrado = textoCifrado + "ober";
+            } else if (texto[i] == "u") {
+                textoCifrado = textoCifrado + "ufat";
+            }else {
+                textoCifrado = textoCifrado + texto[i];
+            }
+            document.querySelector(".text-input-salida").value = textoCifrado;
+            
+        }    
+
+    
+    document.querySelector("#input-texto").value;
 }
 
 var boton1 = document.querySelector("#btn-encriptar");
 boton1.onclick = encriptar;
 
+
 function desencriptar (){
-        var texto = document.querySelector("#input-texto").value;
-        var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
-        document.querySelector(".text-input-salida").value = textoCifrado;
-        document.querySelector("#input-texto").value;
+    var texto = document.querySelector("#input-texto").value;
+    var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    document.querySelector(".text-input-salida").value = textoCifrado;
+    document.querySelector("#input-texto").value;
 
 }
 
 var boton2 = document.querySelector("#btn-desencriptar");
 boton2.onclick = desencriptar;
-
-
-/*function encrypt(input) {
-        array = input.split('');
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] == "a") {
-                array.splice(i, 1, "ai");
-            } else if (array[i] == "e") {
-                array.splice(i, 1, "enter");
-            } else if (array[i] == "i") {
-                array.splice(i, 1, "imes");
-            } else if (array[i] == "o") {
-                array.splice(i, 1, "ober");
-            } else if (array[i] == "u") {
-                array.splice(i, 1, "ufat");
-            }
-        }
-        return array.join("");
-    }
-    
-    function decrypt(input){
-        array = input.split('');
-        for(var i = 0; i < array.length; i++){
-            if(array[i]+array[i+1] == "ai"){
-                array.splice(i, 2, "a");
-            } else if (array[i]+array[i+1]+array[i+2]+array[i+3]+array[i+4] == "enter"){
-                array.splice(i, 5, "e");
-            } else if (array[i]+array[i+1]+array[i+2]+array[i+3] == "imes"){
-                array.splice(i, 4, "i");
-            } else if (array[i]+array[i+1]+array[i+2]+array[i+3] == "ober"){
-                array.splice(i, 4, "o");
-            } else if (array[i]+array[i+1]+array[i+2]+array[i+3] == "ufat"){
-                array.splice(i, 4, "u");
-            }
-        }   
-        return array.join("");
-    }
-    
-*/
-
-
-
-
-
